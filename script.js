805371355d2c917f1aaeb129b9557f5e77864343
Ajay Kumar <109513537+Ajaykumar-1201@users.noreply.github.com>
2023-08-18 18:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-//your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+// Your array of band names
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Nirvana'];
 
+// Function to remove articles from band names
 function removeArticle(name) {
   return name.replace(/^(a|an|the)\s+/i, '');
 }
 
-touristSpots.sort((a, b) => {
+// Sort the band names
+bandNames.sort((a, b) => {
   const nameA = removeArticle(a).toLowerCase();
   const nameB = removeArticle(b).toLowerCase();
   return nameA.localeCompare(nameB);
 });
 
-console.log(touristSpots);
+// Get the ul element by its ID
+const bandList = document.getElementById('band');
+
+// Populate the ul element with sorted band names
+bandNames.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  bandList.appendChild(li);
+});
