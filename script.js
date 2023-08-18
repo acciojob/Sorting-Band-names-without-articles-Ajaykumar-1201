d21e67ccdc490d836394e32ae5940bb8f6e7fe49
Ajay Kumar <109513537+Ajaykumar-1201@users.noreply.github.com>
2023-08-18 18:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArticle(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+touristSpots.sort((a, b) => {
+  const nameA = removeArticle(a).toLowerCase();
+  const nameB = removeArticle(b).toLowerCase();
+  return nameA.localeCompare(nameB);
+});
+
+console.log(touristSpots);
